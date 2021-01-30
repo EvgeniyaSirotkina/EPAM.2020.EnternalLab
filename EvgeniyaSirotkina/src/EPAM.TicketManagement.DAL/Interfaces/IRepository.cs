aa4e5edd509fc6aa42575e156386b4a9519a7b21
YauheniyaSirotkina <zsirotkina@gmail.com>
2021-01-30 23:45:27 +0300

    added interface

---
+++ EvgeniyaSirotkina/src/EPAM.TicketManagement.DAL/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace EPAM.TicketManagement.DAL.Interfaces
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        void Create(T item);
+
+        void Delete(int id);
+
+        void Update(T item);
+
+        T GetById(int id);
+
+        IEnumerable<T> GetAll();
+    }
+}
