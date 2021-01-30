c74e88e3837a4a119d2c933773f9e4fa5a8d0db2
YauheniyaSirotkina <zsirotkina@gmail.com>
2021-01-30 23:39:54 +0300

    added model for Layout entity

---
+++ EvgeniyaSirotkina/src/EPAM.TicketManagement.DAL/Entities/VenueLayout.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketManagement.DAL.Entities
+{
+    public class VenueLayout
+    {
+        public int Id { get; set; }
+
+        public int VenueId { get; set; }
+
+        public string Description { get; set; }
+    }
+}
