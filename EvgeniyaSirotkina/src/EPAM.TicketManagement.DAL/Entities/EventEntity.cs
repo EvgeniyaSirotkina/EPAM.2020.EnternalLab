bc6052dd4e39fd80e28cf8d7c4bee437b25d14d7
YauheniyaSirotkina <zsirotkina@gmail.com>
2021-01-30 23:40:36 +0300

    added model for Event entity

---
+++ EvgeniyaSirotkina/src/EPAM.TicketManagement.DAL/Entities/EventEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TicketManagement.DAL.Entities
+{
+    public class EventEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int LayoutId { get; set; }
+
+        public DateTime EventStart { get; set; }
+
+        public DateTime EventEnd { get; set; }
+    }
+}
