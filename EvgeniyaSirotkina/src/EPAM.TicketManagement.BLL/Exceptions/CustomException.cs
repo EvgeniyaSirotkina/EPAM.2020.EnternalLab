237031ceb73ed1f26da8343c38d10d397631dc0f
YauheniyaSirotkina <zsirotkina@gmail.com>
2021-01-31 17:47:05 +0300

    added custom exception

---
+++ EvgeniyaSirotkina/src/EPAM.TicketManagement.BLL/Exceptions/CustomException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EPAM.TicketManagement.BLL.Exceptions
+{
+    [Serializable]
+    public class CustomException : Exception
+    {
+        public CustomException()
+        {
+        }
+
+        public CustomException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public CustomException(string message)
+            : base(message)
+        {
+        }
+
+        protected CustomException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
+    }
+}
