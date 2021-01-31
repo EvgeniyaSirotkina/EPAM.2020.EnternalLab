679ecdf041c4abec4b0739184878ce393cfbc6d5
YauheniyaSirotkina <zsirotkina@gmail.com>
2021-01-31 17:32:42 +0300

    added public interface

---
+++ EvgeniyaSirotkina/src/EPAM.TicketManagement.BLL/Interfaces/IService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace EPAM.TicketManagement.BLL.Interfaces
+{
+    public interface IService<T>
+        where T : class
+    {
+        void Create(T item);
+
+        void Delete(int id);
+
+        void Update(T item);
+
+        T GetById(int id);
+
+        IEnumerable<T> GetAll();
+    }
+}
